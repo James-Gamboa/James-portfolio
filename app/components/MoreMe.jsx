d7a004d80ba708ea32efb3c28c5b9b402260fc40
James <jjguevarag@gmail.com>
2024-05-20 17:37:39 -0600

    update more me

--- app/components/MoreMe.jsx
+++ app/components/MoreMe.jsx
@@ -6,13 +6,13 @@ function MoreMe() {
           <h1 className="title">About me</h1>
           <div className="flex-container-2">
             <p>
-            Since I entered the world of programming in 2021, I have been an avid learner. 
-            I graduated as a "Technician in web design and development" at the Center for Technology and Visual Arts (CETAV) 
-            I took several courses on platforms such as Udemy , Frontend Masters and FreeCodeCamp , I have applied my knowledge in projects such as a Pokédex , 
-            a recreation of Coca-Cola's website ,Audiophile , an e-commerce platform for audiophiles, 
-            and Travel Haven, an Airbnb clone. In addition, 
-            I have collaborated in innovative projects such as "Human Brush".
-            My passion for programming and my commitment to excellence continue to drive me to explore new horizons and challenges in this exciting field.
+              Since I entered the world of programming in 2021, I have been an avid learner. 
+              I graduated as a &quot;Technician in web design and development&quot; at the Center for Technology and Visual Arts (CETAV).
+              I took several courses on platforms such as Udemy, Frontend Masters, and FreeCodeCamp. I have applied my knowledge in projects such as a Pok&eacute;dex,
+              a recreation of Coca-Cola&apos;s website, Audiophile, an e-commerce platform for audiophiles,
+              and Travel Haven, an Airbnb clone. In addition, 
+              I have collaborated in innovative projects such as &quot;Human Brush&quot;.
+              My passion for programming and my commitment to excellence continue to drive me to explore new horizons and challenges in this exciting field.
             </p>
           </div>
         </div>
